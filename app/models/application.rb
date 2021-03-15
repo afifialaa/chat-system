79492b714a4321515f68ebc8b7d32537a13a7c16
afifialaa <alaaahmedafifi@gmail.com>
2021-03-15 11:39:38 +0200

    count chats in application

--- app/models/application.rb
+++ app/models/application.rb
@@ -4,4 +4,9 @@ class Application < ApplicationRecord
     validates :token, presence: true, uniqueness: true
     validates :name, presence: true
     validates :user_id, presence: true
+
+    def self.count_chats(application_id)
+        sql = "SELECT COUNT(application_id) FROM chats WHERE application_id = #{application_id} "
+        result = ActiveRecord::Base.connection.execute(sql)
+    end
 end
